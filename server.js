5093dfc46f0ecaf8577b8b60397f9f68516ff6a1
Nandhini R <nandhiniraman13497@gmail.com>
2023-12-25 00:57:58 +0530

    create folders

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+app.use(express.json());
+let port = process.env.PORT || 4000;
+
+// method - get
+// public
+// get user
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+  
+app.listen(port, () => console.log(`Server listening on port ${port}`));
